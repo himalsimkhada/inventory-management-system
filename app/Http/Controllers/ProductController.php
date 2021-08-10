fceb1a8eb10b52f3c484d1d1e5f136cd6e7704ad
Himal Simkhada <himalsimkhada88@gmail.com>
2021-08-10 12:03:48 +0545

    .

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        Session::put('admin_page', 'product');
+
+        return view('admin.product.index');
+    }
 }
