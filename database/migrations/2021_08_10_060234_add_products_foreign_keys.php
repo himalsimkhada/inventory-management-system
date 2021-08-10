b10e0a14d223ce342e5bd22bdb54cd973dc20aee
Himal Simkhada <himalsimkhada88@gmail.com>
2021-08-10 11:52:38 +0545

    Tables created for products

---
+++ database/migrations/2021_08_10_060234_add_products_foreign_keys.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProductsForeignKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id');
+
+            $table->foreign('category_id')->references('id')->on('categories');
+
+            $table->unsignedBigInteger('brand_id');
+
+            $table->foreign('brand_id')->references('id')->on('brands');
+
+            $table->unsignedBigInteger('unit_id');
+
+            $table->foreign('unit_id')->references('id')->on('units');
+
+            $table->unsignedBigInteger('tax_type_id');
+
+            $table->foreign('tax_type_id')->references('id')->on('tax_types');
+
+            $table->unsignedBigInteger('product_attribute_id');
+
+            $table->foreign('product_attribute_id')->references('id')->on('product_attributes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
