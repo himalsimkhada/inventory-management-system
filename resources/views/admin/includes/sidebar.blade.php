3070e044a3617f046088f289565bfb1c8d5df401
sovitmaharjan <sovitmaharjan@gmail.com>
2021-08-19 11:35:43 +0545

    qwer

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -94,7 +94,7 @@ $details = \App\Models\Details::where('id', '=', 1)->first();
                                         <path d="M11 4a1 1 0 10-2 0v1a1 1 0 002 0V4zM10 7a1 1 0 011 1v1h2a1 1 0 110 2h-3a1 1 0 01-1-1V8a1 1 0 011-1zM16 9a1 1 0 100 2 1 1 0 000-2zM9 13a1 1 0 011-1h1a1 1 0 110 2v2a1 1 0 11-2 0v-3zM7 11a1 1 0 100-2H4a1 1 0 100 2h3zM17 13a1 1 0 01-1 1h-2a1 1 0 110-2h2a1 1 0 011 1zM16 17a1 1 0 100-2h-3a1 1 0 100 2h3z" />
                                     </svg>
                                 </i>
-                                <span class="ml-2">Generate Barcode</span>
+                                <span class="ml-2">Print Barcode</span>
                             </a>
                         </li>
                     </ul>
