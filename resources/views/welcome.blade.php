7e97cf4746fb8fb905cde45928126ad9f71e0d30
sovitmaharjan <sovitmaharjan@gmail.com>
2021-08-26 12:52:53 +0545

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,11 @@
 @php
 
-
-echo DNS2D::getBarcodeHTML('https://www.google.com/', 'QRCODE');
-echo DNS2D::getBarcodePNGPath('https://www.google.com/', 'PDF417');
-echo DNS2D::getBarcodeSVG('https://www.google.com/', 'DATAMATRIX'); 
-
+// echo DNS2D::getBarcodeHTML('https://www.google.com/', 'QRCODE');
+// echo DNS2D::getBarcodePNGPath('https://www.google.com/', 'PDF417');
+// echo DNS2D::getBarcodeSVG('https://www.google.com/', 'DATAMATRIX'); 
 
 echo '<img src="data:image/png;base64,' . DNS1D::getBarcodePNG('sams-123123', 'C39+',3,33) . '" alt="barcode"   />';
+
+echo DNS1D::getBarcodePNG('sams-123123', 'C39+',3,33);
 
 @endphp
