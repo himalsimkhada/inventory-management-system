b8b90a51ef4e01d8bf4aa7b29c188595d5c468c2
Saral <saral.raut17@gmail.com>
2021-08-29 16:29:17 +0545

    Customer Details Updated

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Yajra\DataTables\DataTables;
 
 class CustomerController extends Controller
 {
     //
     public function index()
     {
+        Session::put('admin_page', 'Customer');
         return view('admin.customer.index');
+    }
+    public function get()
+    {
+        $data = Customer::all()->sortByDesc('id');
+        return DataTables::of($data)
+        ->addIndexColumn()
+        ->make(true);
     }
 }
