cc9fae8d2cf79ea9ba11066235613ce255b5c6b4
himalsimkhada <himalsimkhada88@gmail.com>
2021-08-29 15:40:25 +0545

    Customer add/edit page only

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CustomerController extends Controller
-{
+class CustomerController extends Controller {
     //
-    public function index()
-    {
+    public function index() {
         return view('admin.customer.index');
+    }
+
+    public function addEdit(Request $request) {
+        if ($request->isMethod('post')) {
+            # code...
+        } elseif ($request->isMethod('get')) {
+            return view('admin.customer.addEdit');
+        }
     }
 }
