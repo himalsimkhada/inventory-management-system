db536d65c5d4c1e44b319963c28537c102a8b61b
Saral <saral.raut17@gmail.com>
2021-09-07 13:37:28 +0545

    .

--- routes/web.php
+++ routes/web.php
@@ -93,7 +93,7 @@ Route::prefix('/admin')->group(function () {
         Route::get('/sales','SalesController@index')->name('sales.index');
         Route::get('/sales/get', 'SalesController@show')->name('sales.show');
 
-        Route::resource('sales', 'SalesController');
+        // Route::resource('sales', 'SalesController');
 
         //Product Attribute
         Route::get('/product/{id}/view', 'ProductAttributeController@index')->name('product.attr.index');
