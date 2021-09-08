f8c3c9756dd8e0a8592aa5abaf5ac978806971b0
Saral <saral.raut17@gmail.com>
2021-09-08 13:23:50 +0545

    .

--- resources/views/admin/expenseCategory/addEdit.blade.php
+++ resources/views/admin/expenseCategory/addEdit.blade.php
@@ -5,39 +5,15 @@
         <div class="card">
             <div class="card-body" id="dropzone">
                 <h5 class="font-weight-bold mb-3">Expense Category Information</h5>
-                <form class=" row g-3" method="post"
-                    action="{{ request()->expense_category ? route('expense.update', [request()->expense_category]) : route('expense.store') }}"
-                    enctype="multipart/form-data">
-                    @csrf
-                    <input type="hidden" id="id" name="id" value="">
-                    <div class="col-md-6 mb-3">
-                        <label for="email" class="form-label font-weight-bold text-muted text-uppercase">Code</label>
-                        <input type="email" name="email" id="email" class="form-control"
-                            value="{{ isset($category) ? $category->code : '' }}">
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="phone_number" class="form-label font-weight-bold text-muted text-uppercase">Name</label>
-                        <input type="text" class="form-control" id="phone_number" name="phone_number"
-                            value="{{ isset($category) ? $category->name : '' }}">
-                    </div>
-                    <div class="col-md-12 mb-3">
-                        <button type="submit" class="btn btn-primary" id="submitForm">
-                            @if (request()->expense_category)
-                                Edit
-                            @else
-                                Add
-                            @endif
-                        </button>
-                    </div>
-                    <a href="{{ route('expense_category.index') }}"
-                        class="btn btn-primary btn-sm d-flex align-items-center justify-content-between ml-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd"
-                                d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
-                                clip-rule="evenodd" />
-                        </svg>
-                        <span class="ml-2">Back</span>
-                    </a>
+                <a href="{{ route('expense_category.index') }}"
+                    class="btn btn-primary btn-sm d-flex align-items-center justify-content-between ml-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd"
+                            d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    <span class="ml-2">Back</span>
+                </a>
             </div>
         </div>
         <div class="card">
