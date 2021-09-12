63ed8ab0f6870c8721d568e1fc6e5898288971aa
sovitmaharjan <sovitmaharjan@gmail.com>
2021-09-12 14:34:13 +0545

    changes

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pos;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
@@ -11,13 +12,35 @@ class ReportController extends Controller
     public function saleReport()
     {
         Session::put('admin_page', 'Sales Report');
-        $salesLastWeek = Pos::select('created_at')
-        ->where('created_at', '>', now()->subWeek()->startOfWeek())
-        ->where('created_at', '<', now()->subWeek()->endOfWeek())
-        ->count();
-
-
-        return view('admin.reports.sales-report', compact('salesLastWeek'));
+        $salesLastWeek = 9;
+        $year = 2021;
+        $row = [];
+        for($i=1; $i<=12; $i++){
+            $d = cal_days_in_month(CAL_GREGORIAN, $i, $year);
+            // echo $i . ': ' . $d . '<br>';
+            $from = date($year . '-' . $i . '-' . '01');
+            $to = date($year . '-' . $i . '-' . $d);
+            $quantity = Pos::whereBetween('created_at', [$from, $to])->count();
+            $tax = Pos::whereBetween('created_at', [$from, $to])->sum('tax');
+            $discount = Pos::whereBetween('created_at', [$from, $to])->sum('discount');
+            $total = Pos::whereBetween('created_at', [$from, $to])->sum('total');
+            // echo 'quantity: ' . $quantity . '<br>';
+            // echo 'tax: ' . $tax . '<br>';
+            // echo 'total: ' . $total . '<br>';
+            $month = DateTime::createFromFormat('!m', $i);
+            $row[] = [
+                'month' => $month->format('F'),
+                'quantity' => $quantity,
+                'tax' => $tax,
+                'discount' => $discount,
+                'remaining' => '',
+                'total' => $total,
+            ];
+            // echo $month->format('F');
+            // echo "------------<br>";
+        }
+        // die;
+        return view('admin.reports.sales-report', compact('row'));
     }
 
     public function expenseReport()
